38aed8621484d93e82c70509ef9dd5008134e103
Ryan Overton <ryan.overton@outlook.com>
2020-05-22 17:27:43 -0500

    Move inline styles to class files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { Link, graphql } from "gatsby"
 import dateformat from "../../node_modules/dateformat/lib/dateformat"
+import indexStyles from "../styles/index.module.css"
 
 function getFormattedValue(start, end){              
   let start_date = new Date(start);
@@ -20,47 +21,51 @@ const IndexPage = ({ data }) => (
   <Layout>
     <div className="mdc-layout-grid">
       <div className="mdc-layout-grid__inner"> 
-          <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
-            <h2 style={{color: `#159957`, fontWeight: `normal`, fontSize: `2rem`}}>Blog</h2>
-            <ul style={{listStyleType: `none`, margin: `0`, padding: `0`}}>
+          <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 sectionHeader">
+            <h2>Blog</h2>
+            <ul>
               {data.allKontentItemBlog.edges.map(({ node }) => (
                 <li>
-                <Link style={{fontSize: `1.25rem`, color: `#0275d8`, textDecoration: `none`}} to={node.fields.slug}>
-                  <h3 style={{fontFamily: `"-apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif"`, fontSize: `1.25rem`, color: `#0275d8`, textDecoration: `none`, marginBottom: `0`, fontWeight: `normal`}} href="">{node.elements.blog_title.value}</h3>
-                  <p style={{fontFamily: `"-apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif"`, fontSize: `.75rem`, fontStyle: `italic`}}>{node.elements.publish_date.value}</p>   
+                <Link className="sectionLinks" to={node.fields.slug}>
+                  <h3>{node.elements.blog_title.value}</h3>
+                  <p>{node.elements.publish_date.value}</p>   
                 </Link>               
                 </li>
               ))}
             </ul>
           </div>
-          <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
-          <h2 style={{color: `#159957`, fontWeight: `normal`, fontSize: `2rem`}}>Speaking</h2>
-          <ul style={{listStyleType: `none`, margin: `0`, padding: `0`}}>            
-            {data.allKontentItemSpeakingEngagements.edges.map(({ node }) => (
-              <li>
-                <a style={{fontSize: `1.25rem`, color: `#0275d8`, textDecoration: `none`}} href={node.elements.link_to_event_site.value}>{node.elements.name_of_event.value}</a>
-                <p style={{fontSize: `.75rem`}}><i>{node.elements.location.value}</i>, {getFormattedValue(node.elements.start_date_of_event.value, node.elements.end_date_of_event.value)} </p>
-              </li>                
-            ))}
-          </ul>
+          <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 sectionHeader">
+            <h2>Speaking</h2>
+            <ul>            
+              {data.allKontentItemSpeakingEngagements.edges.map(({ node }) => (
+                <li>
+                  <a className="sectionLinks" href={node.elements.link_to_event_site.value}>
+                    {node.elements.name_of_event.value}
+                  </a>
+                  <p>
+                    <i>{node.elements.location.value}</i>, {getFormattedValue(node.elements.start_date_of_event.value, node.elements.end_date_of_event.value)} 
+                  </p>
+                </li>                
+              ))}
+            </ul>
           </div>
         </div>                
       </div>
       <div className="mdc-layout-grid">
         <div className="mdc-layout-grid__inner">
-          <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-            <h2 style={{color: `#159957`, fontWeight: `normal`, fontSize: `2rem`}}>Live Stream</h2>
+          <div className={"mdc-layout-grid__cell mdc-layout-grid__cell--span-12 sectionHeader " + indexStyles.liveStreamSection}>
+            <h2>Live Stream</h2>
             <div>
               <p>Yep, you heard it right: I've started live streaming!</p>
               <p>We'll be exploring tools, technologies and practices used in software development to make developers more productive <b><i><u>LIVE!</u></i></b></p>
               <ul>
-                  <li style={{marginBottom: `0`}}>Desktop</li>
-                  <li style={{marginBottom: `0`}}>Web</li>
-                  <li style={{marginBottom: `0`}}>Mobile</li>
-                  <li style={{marginBottom: `0`}}>Cloud</li>
-                  <li style={{marginBottom: `0`}}>Agile</li>
-                  <li style={{marginBottom: `0`}}>DevOps</li>
-                  <li style={{marginBottom: `0`}}>...and so on</li>
+                  <li>Desktop</li>
+                  <li>Web</li>
+                  <li>Mobile</li>
+                  <li>Cloud</li>
+                  <li>Agile</li>
+                  <li>DevOps</li>
+                  <li>...and so on</li>
               </ul>
               <span>Come join me on this little adventure on </span> <a href="https://www.twitch.tv/developersgarage">Twitch</a> <span> every Wednesday at 12PM CST. I look forward to seeing you in the chat!</span>
               <br /><br />
@@ -68,10 +73,10 @@ const IndexPage = ({ data }) => (
               <p>Intersted in contributing to the projects, or want to try them for yourself, here's a link to our <a href="https://github.com/devsgarage">GitHub</a> repo where all our work is stored.
               Any, and all, help is greatly appreciated!</p>
               <ul>
-                  <li style={{marginBottom: `0`}}>Documetation</li>
-                  <li style={{marginBottom: `0`}}>styling</li>
-                  <li style={{marginBottom: `0`}}>coding</li>
-                  <li style={{marginBottom: `0`}}>etc.</li>
+                  <li>Documetation</li>
+                  <li>styling</li>
+                  <li>coding</li>
+                  <li>etc.</li>
               </ul>
               <p>It's all fair game, and if you got suggestions for how to improve the stream, or questions in general, you can contact me at any of the methods at the bottom of the page.</p>
             </div>
