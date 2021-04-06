9645821d4b93bc372e21c93c1eb95f21a79d981d
Ryan Overton <ryan.overton@outlook.com>
2021-04-06 12:46:48 -0500

    Add Konent Gatsby plugin beta for new Gatsby Images

--- src/templates/about-template.js
+++ src/templates/about-template.js
@@ -1,67 +1,15 @@
-import { RichTextElement } from "@kentico/gatsby-kontent-components/dist/rich-text-element"
+import { RichTextElement, ImageElement } from "@kentico/gatsby-kontent-components"
+import { GatsbyImage } from "gatsby-plugin-image"
 import { graphql } from 'gatsby'
 import { node } from "prop-types"
 import React from "react"
 import Layout from "../components/layout"
+import * as style from "../styles/about.module.css"
 // import Img from "gatsby-image"
-import { GatsbyImage, getImageData, getImage, generateImageData, getLowResolutionImageURL } from "gatsby-plugin-image"
-const generateImageSource = (baseURL, width, height, format, fit, options) => {
-    const src = `${baseURL}`
-    console.log("baseUrl:" + src)
-    return { src, width, height, format }
-  } 
 
-
-  const resolveGatsbyImageData = async (image, options) => {
-     // The `image` argument is the node to which you are attaching the resolver,
-     // so the values will depend on your data type.
-    const filename = image.url
-  
-    const sourceMetadata = {
-      width: image.width,
-      height: image.height,
-      // In this example, the node has a value like "image/png", which needs
-      // converting to a value such as "png". If this omitted, the function will
-      // attempt to work it out from the file extension.
-      //format: image.mimeType.split("/")[1]
-    }
-  
-    const imageDataArgs = {
-      ...options,
-      // Passing the plugin name allows for better error messages
-      pluginName: `gatsby-custom-code`,
-      sourceMetadata,
-      filename,
-      //placeholderURL,
-      generateImageSource,
-      options,
-    }
-  
-    // Generating placeholders is optional, but recommended
-    //if(options.placeholder === "blurred") {
-      // This function returns the URL for a 20px-wide image, to use as a blurred placeholder
-      // You need to download the image and convert it to a base64-encoded data URI
-      //const lowResImage = getLowResolutionImageURL(imageDataArgs)
-  
-      // This would be your own function to download and generate a low-resolution placeholder
-      //imageDataArgs.placeholderURL =  await getBase64Image(lowResImage)
-    //}
-  
-    // You could also calculate dominant color, and pass that as `backgroundColor`
-    // gatsby-plugin-sharp includes helpers that you can use to generate a tracedSVG or calculate
-    // the dominant color of a local file, if you don't want to handle it in your plugin
-  
-  
-    return generateImageData(imageDataArgs)
-  }
-
-async function getImage2(image) {
-    let t = await resolveGatsbyImageData(image);
-    return getImage(t);
-}
 
 const About = ({data}) => {
-    const richTextElement = data.kontentItemAbout.elements.body
+    const richTextElement = data.kontentItemAbout.elements.body;    
     return(
         <Layout>                
             <div className="mdc-layout-grid">
@@ -87,13 +35,18 @@ const About = ({data}) => {
                         <RichTextElement 
                             value={richTextElement.value} 
                             images={richTextElement.images}
-                            resolveImage={image => {                             
+                            resolveImage={image => {  
                                 return (
-                                    <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 mdc-layout-grid__cell--align-center">                                                                             
-                                        <GatsbyImage image={getImage2(image)}
-                                            imgStyle={{display:`block`, marginLeft: `auto`, marginRight: `auto`, marginBottom: '0', border: `none`}}                                            
-                                            alt={image.description ? image.description : ""}
-                                        />                                        
+                                    <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 mdc-layout-grid__cell--align-center">                                                                                                                                                                    
+                                        <ImageElement
+                                            image={image}
+                                            layout="fixed"
+                                            width={641}
+                                            height={404}
+                                            backgroundColor="#bbbbbb"
+                                            className={style.aboutImage}                                            
+                                            alt={image.description}
+                                        />   
                                         <p style={{textAlign: `center`, margin: `0`}}>{image.description ? image.description : ""}</p>
                                     </div>
                                 )
