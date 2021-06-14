2fb5c5146cf96216f888c4112df12668f9a34ae9
Ryan Overton <ryanoverton@Ryans-MacBook-Pro.local>
2021-06-14 10:24:47 -0500

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,26 @@ module.exports = {
     author: `@ryano9791`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-JSMVQY77JQ", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -39,7 +59,7 @@ module.exports = {
     {
       resolve: `@kentico/gatsby-source-kontent`,
       options: {
-        projectId: `39714182-75f2-0026-3c07-db2a3f970a66`,
+        projectId: `c94fa144-46c2-005b-956e-fc2e2e9d2d21`,
         languageCodenames: [
            `default`,
         ]
